e770da39d4d581f3ecd039a3767e33daf9b654aa
Bo van der Hulst <bohulst@duck.com>
2026-09-09 14:47:09 +0200

    Initial commit: Unreal Engine project setup with Git LFS

---
+++ Source/ThePitProject/ThePitProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ThePitProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThePitProject, "ThePitProject" );
